9ec818cb2a04ad060a0990b71ffff95857ae6aa1
luisbelisario <luisbls77@hotmail.com>
2021-12-29 09:52:06 -0300

    Iniciei a construcao das funcoes de autenticacao

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+import './signup.css';
+import logo from '../../assets/logo.png'
+
+
 function SignUp() {
+    
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert('Clicastes');
+        setEmail('');
+        setPassword('');
+    }
+
     return (
-        <div>
-           <h1>Cadastro</h1>
+        <div className="container-center">
+           <div className="login">
+                <div className="logo-area">
+                    <img src={logo} alt="Logotipo sistema" />
+                </div>
+                <form action="" onSubmit={handleSubmit} className="form-area">
+                    <label htmlFor="">Nome: </label>
+                    <input type="text" value={nome} onChange={ (e) => setNome(e.target.value) } />
+                    <label htmlFor="">Email: </label>
+                    <input type="email" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                    <label htmlFor="">Senha: </label>
+                    <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                    <button type="submit">Cadastrar</button>
+                </form>
+
+                <Link to="/">Já possui uma conta? Entre</Link>
+           </div>
         </div>
     )
 }
